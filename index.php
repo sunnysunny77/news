309bb9074619f4c82cc4d556d92268be3fe60787
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2023-01-25 00:40:25 +0800

    up

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="author" content="D.C">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> News </title>
+    <title> News stories from the west</title>
     <link href="css/app.css" rel="stylesheet" type="text/css">
 </head>
 
